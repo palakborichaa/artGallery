55467a693cf0b66e2687a369e3db96b5fd20ce9e
abd <harshboricha@Palaks-MacBook-Air.local>
2024-04-25 06:52:53 +0530

    aboutus

---
+++ src/Components/Aboutus/Aboutus.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Aboutus = () => {
+  return (
+    <div>
+      <p>HEllo welcome</p>
+    </div>
+  )
+}
+
+export default Aboutus
--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Aboutus from '../Components/Aboutus/Aboutus';
 
 const AboutUs = () => {
   return (
     <div>
-      
+      <Aboutus/>
     </div>
   )
 }
